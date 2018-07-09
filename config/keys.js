696e2c96c28b4abf5285298e8d3a34e4dbf5afc1
SiriusScaper <siriusscaper@gmail.com>
2018-07-09 16:11:21 -0700

    Setup of user authentication using bcrypt and jwt

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://siriusscaper:Gr33n310@ds018508.mlab.com:18508/devconnector-practice'
+  mongoURI: 'mongodb://siriusscaper:Gr33n310@ds018508.mlab.com:18508/devconnector-practice',
+  secretOrKey: 'secret'
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,92 @@
-const express = require('express')
+const express = require('express');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const keys = require('../../config/keys')
 
-const router = express.Router()
+const router = express.Router();
+
+
+// Load User model
+const User = require('../../models/User');
 
 
 // @Route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
-router.get('/test', (req, res) => res.json({ msg: 'Users is working' }))
+router.get('/test', (req, res) => res.json({ msg: 'Users is working' }));
+
+// @Route   GET api/users/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user) {
+        return res.status(400).json({ email: 'Email already exists' });
+      }
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm', //  Default
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password,
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser.save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+
+    });
+});
+
+// @Route   GET api/users/login
+// @desc    Login User/Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email })
+    .then((user) => {
+      // Check
+      if (!user) {
+        return res.status(404).json({ email: 'User not found' });
+      }
+      // Check Password
+      bcrypt.compare(password, user.password)
+        .then((isMatch) => {
+          if (isMatch) {
+            // User matched
+
+            const payload = { id: user.id, name: user.name, avatar: user.avatar } // Create JWT Payload
+
+            // Sign Token
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              { expiresIn: 3600 },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: `Bearer ${token}`,
+                })
+              });
+          } else {
+            return res.status(400).json({ password: 'Password incorrect' });
+          }
+        });
+    });
+});
 
 module.exports = router;
