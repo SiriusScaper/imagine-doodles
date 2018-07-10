6942504d40a4c11fab986dd99bdedf54495d4028
SiriusScaper <siriusscaper@gmail.com>
2018-07-09 18:07:56 -0700

    Added validation for registering users and login. Added profile model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 const keys = require('../../config/keys')
 const passport = require('passport')
 
-const router = express.Router();
-
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
 
 // Load User model
 const User = require('../../models/User');
@@ -21,10 +23,18 @@ router.get('/test', (req, res) => res.json({ msg: 'Users is working' }));
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body)
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
+
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (user) {
-        return res.status(400).json({ email: 'Email already exists' });
+        errors.email = 'Email already exists'
+        return res.status(400).json(errors);
       }
       const avatar = gravatar.url(req.body.email, {
         s: '200', // Size
@@ -54,15 +64,23 @@ router.post('/register', (req, res) => {
 // @desc    Login User/Returning JWT Token
 // @access  Public
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body)
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   // Find user by email
   User.findOne({ email })
     .then((user) => {
-      // Check
+      // Check for user
       if (!user) {
-        return res.status(404).json({ email: 'User not found' });
+        errors.email = 'User not found'
+        return res.status(404).json(errors);
       }
       // Check Password
       bcrypt.compare(password, user.password)
@@ -84,7 +102,8 @@ router.post('/login', (req, res) => {
                 })
               });
           } else {
-            return res.status(400).json({ password: 'Password incorrect' });
+            errors.password = 'Password incorrect'
+            return res.status(400).json(errors);
           }
         });
     });
