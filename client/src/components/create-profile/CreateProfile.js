05557202f48fa7415583b4cbdd72e90c9960e3e4
SiriusScaper <siriusscaper@gmail.com>
2018-07-11 15:47:26 -0700

    Created components for createprofile form

---
+++ client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import TextFieldGroup from '../common/TextFieldGroup'
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup'
+import InputGroup from '../common/InputGroup'
+import SelectListGroup from '../common/SelectListGroup'
+
+class CreateProfile extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      displaySocialInputs: false,
+      handle: '',
+      company: '',
+      website: '',
+      location: '',
+      status: '',
+      skills: '',
+      githubusername: '',
+      bio: '',
+      twitter: '',
+      facebook: '',
+      linkedin: '',
+      youtube: '',
+      instagram: '',
+      errors: {}
+    }
+  }
+  onSubmit = e => {
+    e.preventDefault();
+    console.log('submit')
+  }
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value })
+
+  render() {
+    const { errors } = this.state;
+
+    // Select options for status
+    const options = [
+      { label: '* Select Professional Status', value: 0 },
+      { label: 'Developer', value: 'Developer' },
+      { label: 'Junior Developer', value: 'Junior Developer' },
+      { label: 'Senior Developer', value: 'Senior Developer' },
+      { label: 'Instructor', value: 'Instructor' },
+      { label: 'Manager', value: 'Manager' },
+      { label: 'Student or Learning', value: 'Student or Learning' },
+      { label: 'Insturctor or Teacher', value: 'Insturctor or Teacher' },
+      { label: 'Intern', value: 'Intern' },
+      { label: 'Other', value: 'Other' }
+    ]
+
+    return (
+      <div className='create-profile'>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Create Your Profile</h1>
+              <p className="lead text-center">
+                Let's get some information to make your profile stand out
+              </p>
+              <small className="d-block pb-3">* = Required fields</small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder='* Profile Handle'
+                  name='handle'
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info="A unique handle for your profile URL. Full name, nickname, company, etc"
+                />
+                <SelectListGroup
+                  placeholder='Status'
+                  name='status'
+                  value={this.state.status}
+                  onChange={this.onChange}
+                  error={errors.status}
+                  options={options}
+                  info="Give us an idea of where you are at in your career"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+CreateProfile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, {})(CreateProfile)
