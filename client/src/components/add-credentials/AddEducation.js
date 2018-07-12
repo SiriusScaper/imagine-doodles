f14d4d10b58c306541ec48bdbfb687a416df6ecb
SiriusScaper <siriusscaper@gmail.com>
2018-07-12 16:14:42 -0700

    Added functionality to delete experience and display education

--- client/src/components/add-credentials/AddEducation.js
+++ client/src/components/add-credentials/AddEducation.js
@@ -59,89 +59,90 @@ class AddEducation extends Component {
     const { errors } = this.state
 
     return (
-      <div className='add-education'>
+      <div className="add-education">
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
-              <Link to='/dashboard' className='btn btn-light'>
+              <Link to="/dashboard" className="btn btn-light">
                 Go Back
               </Link>
               <h1 className="display-4 text-center">Add Education</h1>
-              <p className="lead text-center">Add any school, bootcamp, etc that you have attended
+              <p className="lead text-center">
+                Add any school, bootcamp, etc that you have attended
               </p>
               <small className="d-block pb-3">* = required fields</small>
               <form onSubmit={this.onSubmit}>
                 <TextFieldGroup
-                  placeholder='* School'
-                  name='school'
+                  placeholder="* School"
+                  name="school"
                   value={this.state.school}
                   onChange={this.onChange}
                   error={errors.school}
                 />
                 <TextFieldGroup
-                  placeholder='* Degree or Certification'
-                  name='degree'
+                  placeholder="* Degree or Certification"
+                  name="degree"
                   value={this.state.degree}
                   onChange={this.onChange}
                   error={errors.degree}
                 />
                 <TextFieldGroup
-                  placeholder='* Field of Study'
-                  name='fieldofstudy'
+                  placeholder="* Field of Study"
+                  name="fieldofstudy"
                   value={this.state.fieldofstudy}
                   onChange={this.onChange}
                   error={errors.fieldofstudy}
                 />
                 <h6>From Date</h6>
                 <TextFieldGroup
-                  name='from'
-                  type='date'
+                  name="from"
+                  type="date"
                   value={this.state.from}
                   onChange={this.onChange}
                   error={errors.from}
                 />
                 <h6>To Date</h6>
                 <TextFieldGroup
-                  name='to'
-                  type='date'
+                  name="to"
+                  type="date"
                   value={this.state.to}
                   onChange={this.onChange}
                   error={errors.to}
                   disabled={this.state.disabled ? 'disabled' : ''}
                 />
-                <div className='form-check mb-4'>
+                <div className="form-check mb-4">
                   <input
-                    type='checkbox'
-                    className='form-check-input'
-                    name='current'
+                    type="checkbox"
+                    className="form-check-input"
+                    name="current"
                     value={this.state.current}
                     checked={this.state.current}
                     onChange={this.onCheck}
-                    id='current'
+                    id="current"
                   />
                   <label htmlFor="current" className="form-check-label">
-                    Currently Attending
-                </label>
+                    Current Job
+                  </label>
                 </div>
                 <TextAreaFieldGroup
-                  placeholder='Program Description'
-                  name='description'
+                  placeholder="Program Description"
+                  name="description"
                   value={this.state.description}
                   onChange={this.onChange}
                   error={errors.description}
-                  info='Tell us about the position'
+                  info="Tell us about the program that you were in"
                 />
                 <input
                   type="submit"
                   value="Submit"
-                  className='btn btn-info btn-block mt-4'
+                  className="btn btn-info btn-block mt-4"
                 />
               </form>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
 import ProfileActions from './ProfileActions'
 import Experience from './Experience'
+import Education from './Education'
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -32,9 +33,8 @@ class Dashboard extends Component {
             <p className="lead text-muted">Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link></p>
             <ProfileActions />
             {/* TODO: exp and edu */}
-            <Experience
-              experience={profile.experience}
-            />
+            <Experience experience={profile.experience} />
+            <Education education={profile.education} />
             <div style={{ marginBottom: '60px' }} />
             <button onClick={this.onDeleteClick} className='btn btn-danger'>Delete My Account</button>
           </div>
