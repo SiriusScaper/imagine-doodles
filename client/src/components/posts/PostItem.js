d5826c2f70c123a11276197f24321a3f6bad01a2
SiriusScaper <siriusscaper@gmail.com>
2018-07-13 17:24:03 -0700

    Added display of comments, display of num of likes and delete button for the current auth user

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -5,14 +5,43 @@ import classnames from 'classnames'
 import { Link } from 'react-router-dom'
 
 class PostItem extends Component {
+  onDeleteClick = (id) => {
+    console.log(id)
+  }
+
   render() {
-    const { post, atuh } = this.props
+    const { post, auth } = this.props
 
     return (
       <div className="card card-body mb-3">
         <div className="row">
           <div className="col-md-2">
-
+            <a href="profile.html">
+              <img src={post.avatar} alt="" className="rounded-circle d-none d-md-block" />
+            </a>
+            <br />
+            <p className="text-center">{post.name}</p>
+          </div>
+          <div className="col-md-10">
+            <p className="lead">{post.text}</p>
+            <button className="btn btn-light mr-1">
+              <i className="text-info fas fa-thumbs-up" />
+              <span className="badge bade-light">{post.likes.length}</span>
+            </button>
+            <button className="btn btn-light mr-1">
+              <i className="text-info fas fa-thumbs-up" />
+              <span className="badge bade-light">{post.likes.length}</span>
+            </button>
+            <button className="btn btn-light mr-1">
+              <Link to={`/post/${post._id}`} className='btn btn-info mr-1'>
+                Comments
+              </Link>
+              {post.user === auth.user.id ? (
+                <button onClick={this.onDeleteClick(post._id)} type='button' className="btn btn-danger mr-1">
+                  <i className="fas fa-times" />
+                </button>
+              ) : null}
+            </button>
           </div>
         </div>
       </div>
